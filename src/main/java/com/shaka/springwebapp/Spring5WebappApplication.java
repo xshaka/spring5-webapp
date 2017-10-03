3d0fad1449cec519597e49cf574557b18b01ed59
xshaka <shaka.shadows@gmail.com>
2017-10-02 20:09:06 -0700

    Initial commit with repository base status.

---
+++ src/main/java/com/shaka/springwebapp/Spring5WebappApplication.java
@@ -0,0 +1,12 @@
+package com.shaka.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shaka/springwebapp/Spring5WebappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shaka.springwebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5WebappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
